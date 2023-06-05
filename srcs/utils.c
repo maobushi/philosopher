19fee9ae1afdd0a95103d897cf37952e08e07506
Masayasu Obushi <mobushi@c1r27s10.42tokyo.jp>
2023-06-05 16:34:13 +0900

    save

--- srcs/utils.c
+++ srcs/utils.c
@@ -1,50 +1,15 @@
 #include "../include/philosopher.h"
 
 
-void philosopher_eating(long time,size_t index)
+void thread_function(void *input)
 {
-	printf("%ld %zu is eating\n",time,index);
+	pthread_mutex
 }
 
-void philosopher_thinking(long time,size_t index)
+void init_philosophers(t_input_env *env)
 {
-	printf("%ld %zu is sleeping\n",time,index);
-}
-
-void philosopher_sleeping(long time,size_t index)
-{
-	printf("%ld %zu is thinking\n",time,index);
-}
-
-
-void do_philosopher(void *input)
-{
-	t_input_env *env = (t_input_env *)input;
-	gettimeofday(&current_time,NULL);
-
-	pthread_mutex_lock(&env->fork);
-	if(env->index % 2 == 1)
-		usleep(100);
-	philosopher_eating(current_time.tv_sec,env->index);
-	philosopher_thinking(current_time.tv_sec,env->index);
-	philosopher_sleeping(current_time.tv_sec,env->index);
-	pthread_mutex_unlock(&env->fork);
-}
-
-void init_philosophers(t_input_env * env)
-{
-	size_t i;
-	i = 0;
-
-	while(i < env->number_of_philosophers)
-	{
-		env->index = i;
-		env->fork = env->forks[i];
-		pthread_mutex_init(&env->forks[i], NULL);
-		pthread_create(&env->forks[i],NULL,do_philosopher,&env);
-		i++;
-	}
-
-	
-
+	pthread_t thread[100];
+	pthread_t mutex[100];
+	pthread_mutex_init(mutex)
+	pthread_create(&thread[i],NULL,)
 }
