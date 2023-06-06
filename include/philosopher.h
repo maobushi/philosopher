e5847fcaade649a974457d60314492cc7a639138
Masayasu Obushi <mobushi@c1r27s8.42tokyo.jp>
2023-06-06 23:11:04 +0900

    add philo function

--- include/philosopher.h
+++ include/philosopher.h
@@ -5,27 +5,28 @@
 #include<stdbool.h>
 #include <pthread.h>
 #include <sys/time.h>
-#define NUM_OF_THREAD 4
+#define NUM_OF_THREAD 3
 
 
 typedef struct{
-	
-	pthread_mutex_t	*forks;
-	pthread_mutex_t	fork;
+	size_t	index;
+	size_t	thread_index;
+	long	initial_time;
+	bool	fork_status;
+	pthread_mutex_t mutex_id;
+	pthread_t	thread_id;
+	struct s_env	*env;
 }t_philo_env;
 
 
-typedef struct{
-	t_philo_env	philo;
+typedef struct s_env{
+	t_philo_env	philo[NUM_OF_THREAD ];
 	size_t	number_of_philosophers;
-	size_t	index;
 	size_t	time_to_die;
 	size_t	time_to_eat;
 	size_t	time_to_sleep;
 	size_t	number_of_times_each_philosopher_must_eat;
 	long	initial_time;
-	pthread_mutex_t mutex_id[NUM_OF_THREAD];
-	pthread_t	thread_id[NUM_OF_THREAD];
 }t_env;
 
 
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,66 +6,99 @@ long ft_gettime(void)
 	return (current_time.tv_sec);	
 }
 
-void eat(t_env *env)
+void p_eat(t_philo_env *env)
 {
-	pthread_mutex_lock(&(env->mutex_id[env->index]));
-	printf("this thread number is:%zu\n",env->index);
-	printf("initial_time:%ld\n",env->initial_time);
-	printf("current_time:%ld\n",ft_gettime());
-	pthread_mutex_unlock(&(env->mutex_id[env->index]));
-
+	pthread_mutex_lock(&(env->mutex_id));
+	if(env->env->philo[env->index].index == NUM_OF_THREAD - 1)
+				env->env->philo[env->index + 1].fork_status = env->env->philo[0].fork_status;
+	else if(env->env->philo[env->index].index == 0)
+				env->env->philo[0].fork_status = env->env->philo[env->index + 1].fork_status;
+	
+	if(env->fork_status == false)
+	{
+		printf("%ld ",ft_gettime() - env->initial_time);
+		printf("%zu has taken a fork[%zu]\n",env->index,env->index);
+		env->fork_status = true;
+		//env->env->philo[env->index + 1].index
+		if(env->fork_status == true && env->env->philo[env->index + 1].fork_status == false)
+		{
+			printf("%ld ",ft_gettime() - env->initial_time);
+			printf("%zu has taken a fork[%zu]\n",env->index,env->env->philo[env->index + 1].index);
+			env->env->philo[env->index + 1].fork_status = true;
+			
+			printf("%ld ",ft_gettime() - env->initial_time);
+			printf("%zu is eating\n",env->index);
+		}
+	}
+	pthread_mutex_unlock(&(env->mutex_id));
 }
 
-
+void p_sleep(t_philo_env *env)
+{
+	if(env->env->philo[env->index].fork_status == true && env->env->philo[env->index + 1].fork_status == true)
+	{
+		printf("%ld %zu is sleeping\n",ft_gettime() - env->initial_time,env->index);
+		sleep(1);
+		pthread_mutex_lock(&(env->mutex_id));
+		env->env->philo[env->index].fork_status = false;
+		env->env->philo[env->index + 1].fork_status = false;
+		pthread_mutex_unlock(&(env->mutex_id));
+		printf("%ld %zu is wake up\n",ft_gettime() - env->initial_time,env->index);
+	}
+}
 
 void *thread_function(void *input)
 {
-	t_env * env;
-	env = (t_env *)input;
+	t_philo_env * env;
+	env = (t_philo_env *)input;
 
-	printf("thread_function!\n");
-	eat(env);
-	// sleep(input);
+	while(1)
+	{
+		if(env->index % 2 == 1)
+		{
+			usleep(20000);
+			//printf("this thread_id is %zu\n",env->index);
+			//return NULL;
+		}
+		p_eat(env);
 
+
+		p_sleep(env);
+	}
 	return NULL;
 }
 
 void init_philosophers(t_env *env)
 {
-	// pthread_t mutex[100];
-	printf("hello:%ld\n",env->time_to_die);
 	env->initial_time = ft_gettime();
 
-	printf("your current time is:%ld\n",env->initial_time);
+	//printf("your current time is:%ld\n",env->initial_time - ft_gettime());
+	printf("======================\n");
 	size_t i = 0;
 
 	while(i < NUM_OF_THREAD)
 	{
-		env->index = i;
-		pthread_mutex_init(&(env->mutex_id[i]),NULL);
-		pthread_create(&(env->thread_id[i]),NULL,thread_function,env);
+		env->philo[i].initial_time = env->initial_time;
+		env->philo[i].index = i;
+		env->philo[i].fork_status = false;
+		env->philo[i].env = env;
+
+		pthread_mutex_init(&(env->philo[i].mutex_id),NULL);
+		pthread_create(&(env->philo[i].thread_id),NULL,thread_function,&env->philo[i]);
+		//sleep(1);
 		i++;
 	}
-
-	//init each philosopher info
-	// size_t i;
-	// i = 0;
-	// while(i < env->number_of_philosophers)
-	// {
-	// 	env->philo.
-	// 	i++;
-	// }
 }
 
 void end_philosophers(t_env *env)
 {
-	usleep(100);
+	sleep(1000);
 	size_t i = 0;
 	printf("end_philosophers\n");
 	while(i < NUM_OF_THREAD)
 	{
-		pthread_mutex_destroy(&(env->mutex_id[i]));
-		pthread_join((env->thread_id[i]),NULL);
+		pthread_mutex_destroy(&(env->philo[i].mutex_id));
+		pthread_join((env->philo[i].thread_id),NULL);
 		i++;
 	}
 }
